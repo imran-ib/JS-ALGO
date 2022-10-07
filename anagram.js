9f29ab887598a7e472b058639117fe4507f65874
imran <imran.ib@live.com>
2022-10-07 21:59:28 +0300

    more complex code for anasgram

--- anagram.js
+++ anagram.js
@@ -2,7 +2,7 @@ function anagram(str1, str2) {
   // check if two strings are of same length
   const isSameLength = str1.length === str2.length;
   if (!isSameLength) {
-    return;
+    return `Not Anagram`;
   }
   // sort the strings
   const str1Sorted = str1.split("").sort().join("");
@@ -15,4 +15,34 @@ function anagram(str1, str2) {
   return true;
 }
 
-console.log(anagram("room", "moor"));
+// console.log(anagram("room", "moor"));
+
+function anagram2(str1, str2) {
+  if (str1.length !== str2.length) {
+    return `Not Anagram`;
+  }
+  // construct an object
+  let lookUpValue = {};
+
+  for (let i = 0; i < str1.length; i++) {
+    let letter = str1[i];
+    lookUpValue[letter]
+      ? (lookUpValue[letter] += 1)
+      : (lookUpValue[letter] = 1);
+  }
+  // loop over second string and compare it
+
+  for (i = 0; i < str2.length; i++) {
+    let letter = str2[i];
+    // if letter does not match return false
+    if (!lookUpValue[letter]) {
+      return false;
+    } else {
+      // decrease
+      lookUpValue[letter] -= 1;
+    }
+  }
+  return true;
+}
+
+console.log(anagram2("moore", "eroom"));
