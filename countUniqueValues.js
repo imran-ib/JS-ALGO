d7b58500ec6085c18916fbcc7f81f52f29268d7a
imran <imran.ib@live.com>
2022-10-09 20:31:39 +0300

    Count unique Values

---
+++ countUniqueValues.js
@@ -0,0 +1,12 @@
+function CountUniqueValues(arr) {
+  let i = 0;
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+    console.log(i, j)
+  }
+}
+
+console.log(CountUniqueValues([1, 1, 2, 2, 3.3, 4, 4, 5, 5, 6, 6]));
