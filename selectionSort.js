4bede1bc249829020cf5c7d99e0239d9d320fa7f
imran <imran.ib@live.com>
2022-10-19 23:09:50 +0300

    selectionSort-start

---
+++ selectionSort.js
@@ -0,0 +1,3 @@
+// similar to bubble sort but instead of checking two values and swapping
+// we will go through data and find and place smallest value at beginning.
+// we keep doing that until array is sorted
