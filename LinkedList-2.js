17fd4e2dda49bfac024fe2aad2148f19f8721091
imran <imran.ib@live.com>
2022-12-23 00:20:43 +0300

    linkedlist-2

--- LinkedList-2.js
+++ LinkedList-2.js
@@ -86,7 +86,85 @@ class LinkedList {
   }
 
   // take a index and get that node
-  get(index) {}
+  get(index) {
+    if (index < 0 || index >= this.length) {
+      return undefined;
+    }
+    console.log(index);
+    let count = 0;
+    let node = this.head;
+
+    while (node) {
+      if (count === index) {
+        return node;
+      }
+      node = node.next;
+      count++;
+    }
+    return null;
+  }
+  // update existing node
+  set(index, data) {
+    let node = this.get(index);
+    if (node) {
+      node.node = data;
+      return true;
+    }
+    return false;
+  }
+
+  insert(index, data) {
+    if (index < 0 || index > this.length) {
+      return false;
+    }
+    if (index === this.length) {
+      return !!this.push(data);
+    }
+    if (index === 0) {
+      return !!this.unShift(data);
+    }
+    let node = new Node(data);
+    let prevNode = this.get(index - 1);
+    let temp = prevNode.next;
+    prevNode.next = node;
+    node.next = temp;
+    this.length++;
+    return true;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.length) {
+      return null;
+    }
+    if (index === 0) {
+      return this.shift();
+    }
+    if (index === this.length - 1) {
+      return this.pop();
+    }
+
+    let prevNode = this.get(index - 1);
+    let removingNode = prevNode.next;
+    prevNode.next = removingNode.next;
+    this.length--;
+    return removingNode;
+  }
+
+  reverse() {
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    let next = null;
+    let prev = null;
+
+    for (let i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+    return this;
+  }
 }
 
 const list = new LinkedList();
@@ -94,3 +172,4 @@ list.push("First item");
 list.push("Second item");
 list.push("Third item");
 list.push("Fourth item");
+list.push("Fifth item");
